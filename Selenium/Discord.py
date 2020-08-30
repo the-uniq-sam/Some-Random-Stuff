8be95d44d3c18e98b1cd87b7dc866d66a6458fff
Samyak Jain <samyakjain455@gmail.com>
2020-08-30 19:40:59 +0530

    Update Discord.py

--- Selenium/Discord.py
+++ Selenium/Discord.py
@@ -9,11 +9,15 @@ driver.find_element_by_name("email").send_keys("email here")
 driver.find_element_by_name("password").send_keys("password here")
 driver.find_element_by_xpath("//button[@type='submit']").click()
 #Now goto desired channel manually
-print("Now goto desired channel manually")
-time.sleep(30)
-msg = driver.find_element_by_xpath("//div[@aria-label='Message #genereal']")
-a = 1
-while a==1:
+a = 'n'
+print("Now goto desired channel manually making sure typebox says Message #general and press y when done")
+while(a!='y')
+  a = input()
+  print("Now goto desired channel manually making sure typebox says Message #general and press y when done")
+time.sleep(3)
+msg = driver.find_element_by_xpath("//div[@aria-label='Message #general']")
+b = 1
+while b==1:
   msg.send_keys("Pls beg")
   msg.send_keys(Keys.ENTER)
   time.sleep(30)
@@ -21,4 +25,4 @@ while a==1:
   msg.send_keys(Keys.ENTER)
   time.sleep(11)
 
-print("Script is no working, no need to print")
+print("Script is now working, no need to print you noob")
