31159c505ba4b346bc36a101e3a64bf7765a1cc6
Samyak Jain <samyakjain455@gmail.com>
2020-08-30 20:11:28 +0530

    Update Discord.py

--- Selenium/Discord.py
+++ Selenium/Discord.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+
 driver = webdriver.Firefox()
 time.sleep(15)
 driver.get("http://www.discord.com/login")
 time.sleep(30)
-driver.find_element_by_name("email").send_keys("email here")
-driver.find_element_by_name("password").send_keys("password here")
+driver.find_element_by_name("email").send_keys("Enter your EMAIL here")
+driver.find_element_by_name("password").send_keys("Enter your PASSWORD here")
 driver.find_element_by_xpath("//button[@type='submit']").click()
+
 #Now goto desired channel manually
 a = 'n'
 print("Now goto desired channel manually making sure typebox says Message #general and press y when done")
@@ -15,6 +17,7 @@ while(a!='y')
   a = input()
   print("Now goto desired channel manually making sure typebox says Message #general and press y when done")
 time.sleep(3)
+
 msg = driver.find_element_by_xpath("//div[@aria-label='Message #general']")
 b = 1
 while b==1:
